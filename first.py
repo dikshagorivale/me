62dea30989013e276a72c3af28a560dd7246b1d2
Your Name <you@example.com>
2024-06-08 12:29:41 +0530

    added new file

---
+++ first.py
@@ -0,0 +1,164 @@
+# # print("hello world")
+# # list=[12,"abc",567,"agag",45,"abc"]
+# # a=12
+# # b="bfgd"
+# # c=65.6
+# # x=1,2,3.5,"abc"
+# # y={2,4.6,"dghgf",45,"gghgh"}
+# # print(list)
+# # print(type(list))
+# # print(type(a))
+# # print(type(b))
+# # print(type(c))
+# # print(type(x))
+# # print(type(y))
+# # print(x)
+# # print(y)
+
+# # school={
+# #     "stud_name":"ghgf",
+# #     "roll_no":"33"
+# #        }
+# # print(school)
+# # print(type(school))
+
+
+# # a=9
+# # b=8
+# # c=a+b
+# # print(c)
+# # d=a-b
+# # print(d)
+# # e=a*b
+# # print(e)
+# # f=a/b
+# # print("division is:",f, "add is :",c)
+
+# # print(f"add is {c} sub is {d} div is {f} mul is {e}")
+
+# # #sqr and raides to values
+# # #x=9**73
+# # x=73//9
+# # print(x)
+
+
+# # if a>b:
+# #  print(f"{a} is max ")
+
+
+# # #print(a!=b)
+# # #print(a<b)
+# # print(a>b)
+# # print(a==b)
+
+# # if a>b or a!=9 :
+# #     print(a)
+
+# # if a>b  and a!=9 :
+# #     print(a)
+
+
+# # x=False
+# # y=True
+
+# # print(x and y)
+
+# # if x and y:
+# #     print(x or y)
+
+# # if x != y:
+# #     print(x != y)
+
+# # a=5
+# # print(a>>2)
+# # print(a<<2)
+
+# # if a is not 5:
+# #     print("a is 5")
+
+
+# # ##control flow statements
+# # x=9
+# # if (x==9):
+# #     print(x is 9)
+# # elif (x>9):
+# #     print("x is 9")
+# # else :
+# #     print("x is not 9")
+
+# # a=59
+# # if (60>a>10):
+# #     print("True")
+# # else:
+# #         print("False")
+
+# # z=float(input("number: "))
+# # print(type(z))
+
+
+# # a=int(input("a is: "))
+# # b=int(input("b is: "))
+# # c=int(input("c is: "))
+# # if (c==1):
+# #     d=a+b
+# #     print(d)
+# # elif (c==2):
+# #     d=a-b
+# #     print(d)
+    
+# # elif (c==3):
+# #     d=a*b
+# #     print(d)
+    
+# # elif (c==4):
+# #     d=a/b
+# #     print(d)
+    
+# # else:
+# #     print("invalid")
+
+
+# # for i in range(1,10,3):
+# #     print(i)
+
+
+
+    
+# # list=[1,9,3,2]
+# # n=(len(list))
+# # print(n)    
+# # med=(n+1)/2
+# # a=int(med-1)
+# # median=list[a]
+# # print(median)
+
+# # #sum of list
+# # list=[1,9,3,2]
+# # sum=0
+# # for i in range(len(list)):
+# #     sum+=list[i]
+# #     print(sum)
+
+
+# # i=1
+# # while(i<5):
+# #     print(i)
+# #     i+=1
+    
+# i=0
+# pass1=123
+# user_input=int(input("Enter pass"))
+# if (pass1==user_input):
+#     print("Unlock")
+# else:
+#     while(i<5):
+#         user_input=int(input("Enter pass"))
+#         if (pass1==user_input):
+#             print("unlock")
+#             break;
+#         i+=1
+#     else:
+#         print("Phone lock for 1 minute")
+
+
+
